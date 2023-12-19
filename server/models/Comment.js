83e9c7dc2a360597bcb76cb8a613a9aa88ec8c79
Lê Quang Doanh <82566414+lequangdoanh@users.noreply.github.com>
2023-12-20 03:00:44 +0700

    models cmt
    update md cmt

---
+++ server/models/Comment.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema({
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", 
+        required: true,
+      },
+    comment: {
+        type: String,
+        required: true,
+    },
+    blogId: {
+        type: String,
+    },
+},
+    {
+        timestamps: true,
+    }
+);
+
+export default mongoose.model("Comment", CommentSchema);
